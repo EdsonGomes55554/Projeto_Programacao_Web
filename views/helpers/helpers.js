d824c1aef1fe8bc33a7defb83b7ce970f4de0596
Edson Gomes <51749663+EdsonGomes55554@users.noreply.github.com>
2020-06-03 16:30:15 -0300

    Adicionados arquivos da handlesbars

---
+++ views/helpers/helpers.js
@@ -0,0 +1,10 @@
+function section(name, options){
+    if(!this._sections) this._sections = {};
+    this._sections[name] = options.fn(this);
+    return null;
+}
+
+
+module.exports = {
+    section: section
+}
